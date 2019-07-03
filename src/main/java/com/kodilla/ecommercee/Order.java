62fe2473b0d67ca81e0cb3236c7b9b396c779abc
wojtekcywinski <wojciech.cywinski@gmail.com>
2019-07-03 19:39:55 +0200

    Order entity - JDP190602-11

---
+++ src/main/java/com/kodilla/ecommercee/Order.java
@@ -0,0 +1,43 @@
+package com.kodilla.ecommercee;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Getter
+@Table(name = "order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @NotNull
+    private Long id;
+
+    @OneToMany(
+            targetEntity = User.class,
+            mappedBy = "order",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    private Long userId;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "CART_ID")
+    private Cart cartId;
+
+    private List<Item> products = new ArrayList<>();
+    private LocalDateTime orderDate;
+    private OrderStatus orderStatus;
+    private BigDecimal totalOrder;
+}
+
