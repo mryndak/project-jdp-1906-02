4bb1dbe1d65afc565345c0a1b8cfe43974628a1c
Joanna Zawiślak <asia.greczka@gmail.com>
2019-07-03 23:06:46 +0200

    User Entity - JDP190602-15

--- src/main/java/com/kodilla/ecommercee/GroupRepository.java
+++ src/main/java/com/kodilla/ecommercee/GroupRepository.java
@@ -1,8 +1,7 @@
-package com.kodilla.ecommercee.repository;
+package com.kodilla.ecommercee;
 
-import com.kodilla.ecommercee.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GroupRepository extends JpaRepository<Group, Long> { }
+public interface GroupRepository extends JpaRepository<GroupEntity, Long> { }
