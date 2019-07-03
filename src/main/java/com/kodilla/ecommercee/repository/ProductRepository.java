2d5877e61fd5446f60e944ec30043fbafe5283b0
Joanna Zawiślak <asia.greczka@gmail.com>
2019-07-03 22:51:33 +0200

    Product entity - JDP190602-12

--- src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
@@ -1,6 +1,6 @@
 package com.kodilla.ecommercee.repository;
 
-import com.kodilla.ecommercee.domain.ProductEntity;
+import com.kodilla.ecommercee.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
