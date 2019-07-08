cda7af579f885270cb754c518b1e2319b9b37909
Joanna Zawiślak <asia.greczka@gmail.com>
2019-07-08 12:15:44 +0200

    Controllers refactoring - JDP190602-29

--- src/main/java/com/kodilla/ecommercee/controller/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/controller/OrderController.java
@@ -12,32 +12,32 @@ import static com.kodilla.ecommercee.domain.OrderStatus.*;
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
 @RestController
-@RequestMapping("/v1/ecommercee")
+@RequestMapping("/v1/ecommercee/users")
 public class OrderController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "getAllOrders")
-    public List<OrderDto> getAllOrders() {
+    @RequestMapping(method = RequestMethod.GET, value = "{userId}/orders")
+    public List<OrderDto> getAllOrders(@PathVariable Long userId) {
         List<OrderDto> orderList = new ArrayList<>();
         orderList.add(new OrderDto(1L, 1L, 1L,  new ArrayList<>(), LocalDateTime.of(2019,6,25,16,49), CREATED, new BigDecimal(4)));
         orderList.add(new OrderDto(2L, 2L, 2L,  new ArrayList<>(), LocalDateTime.of(2019,6,26,17,49), CREATED, new BigDecimal(17)));
         return orderList;
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "createOrder", consumes = APPLICATION_JSON_VALUE)
-    public void createOrder(@RequestBody OrderDto orderDto) {
+    @RequestMapping(method = RequestMethod.POST, value = "{userId}/orders", consumes = APPLICATION_JSON_VALUE)
+    public void createOrder(@PathVariable Long userId, @RequestBody OrderDto orderDto) {
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getOrder")
-    public OrderDto getOrder(@RequestParam Long orderId) {
+    @RequestMapping(method = RequestMethod.GET, value = "{userId}/orders/{orderId}")
+    public OrderDto getOrder(@PathVariable Long userId, @PathVariable Long orderId) {
         return new OrderDto(1L, 1L, 1L,  new ArrayList<>(), LocalDateTime.of(2019,6,25,16,49), CREATED, new BigDecimal(4));
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "updateOrder")
-    public OrderDto updateOrder(@RequestBody OrderDto orderDto) {
+    @RequestMapping(method = RequestMethod.PUT, value = "{userId}/orders")
+    public OrderDto updateOrder(@PathVariable Long userId, @RequestBody OrderDto orderDto) {
         return new OrderDto(1L, 1L, 1L,  new ArrayList<>(), LocalDateTime.of(2019,6,25,16,55), PAID, new BigDecimal(4));
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "deleteOrder")
-    public void deleteOrder(@RequestParam Long orderId) {
+    @RequestMapping(method = RequestMethod.DELETE, value = "{userId}/orders/{orderId}")
+    public void deleteOrder(@PathVariable Long userId, @PathVariable Long orderId) {
     }
 }
