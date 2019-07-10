c933dcc1b138e75521d41b0213ad029ef2525137
psheemas <psheemas@gmail.com>
2019-07-10 22:13:44 +0200

    JDP190602-22

--- src/main/java/com/kodilla/ecommercee/dto/GroupDto.java
+++ src/main/java/com/kodilla/ecommercee/dto/GroupDto.java
@@ -10,4 +10,5 @@ import lombok.NoArgsConstructor;
 public class GroupDto {
     private Long id;
     private String name;
+
 }
---
+++ src/main/java/com/kodilla/ecommercee/service/GroupService.java
@@ -0,0 +1,43 @@
+package com.kodilla.ecommercee.service;
+
+import com.kodilla.ecommercee.domain.GroupEntity;
+import com.kodilla.ecommercee.dto.GroupDto;
+import com.kodilla.ecommercee.mapper.GroupMapper;
+import com.kodilla.ecommercee.repository.GroupRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GroupService {
+
+    @Autowired
+    private GroupRepository groupRepository;
+
+    @Autowired
+    GroupMapper groupMapper;
+
+    public List<GroupDto> getAllGroups(){
+        List<GroupEntity> groups = groupRepository.findAll();
+        return groupMapper.mapToGroupDtoList(groups);
+    }
+
+    public GroupDto getGroupById(Long groupId){
+        GroupEntity group = groupRepository.getById(groupId).get();
+        return groupMapper.mapToGroupDto(group);
+    }
+
+    public GroupDto saveGroup(GroupDto groupDto){
+        GroupEntity group = groupMapper.mapToGroupEntity(groupDto);
+        return groupMapper.mapToGroupDto(groupRepository.save(group));
+    }
+
+    public void deleteGroup(Long groupId){
+        groupRepository.deleteById(groupId);
+    }
+
+    public boolean isExist(Long groupId){
+        return groupRepository.existsById(groupId);
+    }
+}
